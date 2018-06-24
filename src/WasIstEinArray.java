af9c8dbab1e7bc651fbdcb5a3697759d183fbf0d
Patrick Jansen <pjansen@sunset-design.de>
2018-06-24 22:13:50 +0200

    commit

---
+++ src/WasIstEinArray.java
@@ -0,0 +1,26 @@
+
+public class WasIstEinArray {
+
+	public static void main(String[] args) {
+		
+		// Variablen Deklaration
+		// Datentyp Name = Wert;
+		
+		int zahl = 50;
+		double zahl1;
+		
+		// Array Deklaration
+		// Datentyp [] Name = new Datentyp[Anzahl Einträge]
+		
+		int [] zahlenArray = new int [7];
+		zahlenArray[3] = 50;
+		zahlenArray[5] = 500;
+		zahlenArray[0] = 40;
+		
+		double [] zahlenArray1 = new double[7];
+		zahlenArray1[1] = 34.23;
+		zahlenArray1[4] = 50.45;
+
+	}
+
+}
